920490afcae17879d413ff3b311e37e7ae1a31e0
manuel <46604810+ManuelDelpero@users.noreply.github.com>
2019-09-25 11:25:24 +0200

    small updates

--- QualityControl/PCA.R
+++ QualityControl/PCA.R
@@ -3,7 +3,8 @@
 # copyright (c) - Manuel Delpero
 # first written April, 2019
 #
-# Script for the analysis on the selected animals to genotype
+# Script for the analysis on the selected animals to genotype with gigaMUGA array
+# Have a look using PCA if the sample selected cover the variability of the popultaion
 
 setwd("C:/Users/Manuel/Desktop/AIL_S1xS2/RAWDATA")
 mriLEAN <- read.csv("MRIlean.txt", sep = "\t", header=TRUE, check.names=FALSE)
@@ -11,14 +12,7 @@ mriFAT <- read.csv("MRIfat.txt", sep = "\t", header=TRUE, check.names=FALSE)
 allPhenotypes <- read.csv("allPhenotypes.txt",sep = "\t", header=TRUE, check.names=FALSE, na.strings=c("NA", "", "-", "    ", "x"), colClasses="character")
 extremes <- read.csv("groups.txt" ,sep = "\t", header=TRUE, check.names=FALSE)
 
-# PCA with the extremes for Gon Fat and Triglycerides
-GonTrig <- allPhenotypes[, c("Gon", "Triglycerides")]
-GonTrig <- GonTrig[rownames(GonTrig) %in% extremes[,1],]
-out <- which(is.na(GonTrig[,1]))
-GonTrigEx <- GonTrig[-out,]
-pcaGonTrigEx <- prcomp(GonTrigEx)
-plot(pcaGonTrigeEx$x[,1], pcaGonTrigEx$x[,2])
-
+# PCA with all the phenotypes
 subsetPCA <- allPhenotypes[which(apply(apply(allPhenotypes,1,is.na),2,sum) == 0), -c(1,2)]
 subsetPCAnum <- apply(subsetPCA, 1, function(x){as.numeric(as.character(x))})
 pcares <- prcomp(t(subsetPCAnum))
@@ -42,7 +36,7 @@ plot(main = "PCA-Plot all phenotypes", pcares$x[,1], pcares$x[,2], xlab = "PC1 (
 
 dev.off()
 
-# PCA with the extremes for all the samples
+# PCA with 2 variables (Gon Fat and liver Triglycerides)
 GonTrig <- allPhenotypes[, c("Gon", "Triglycerides")]
 out1 <- which(is.na(GonTrig[,"Gon"]))
 out2 <- which(is.na(GonTrig[,"Triglycerides"]))
@@ -51,7 +45,6 @@ pcaGonTrig <- prcomp(GonTrig)
 plot(pcaGonTrig$x[,1], pcaGonTrig$x[,2])
 
 pdf ("PCA-plot.pdf")
-# Plot for PCA with all samples highlighting the selected animals
 plot(main = "2D PCA-plot with all animals", x = c(min(pcaGonTrig$x[,1]), max(pcaGonTrig$x[,1])), y = c(min(pcaGonTrig$x[,2]), max(pcaGonTrig$x[,2])), type='p', pch=20, xlab='Comp.1(Triglycerides)', ylab='Comp.2(Gonadal Fat)')
   for (num in 1:length(pcaGonTrig$x[,1])){
     if (rownames(GonTrig[num,]) %in% rownames(GonTrigEx) == TRUE){
@@ -72,5 +65,3 @@ p.variance.explained = p$sdev^2 / sum(p$sdev^2)
  
 # plot percentage of variance explained for each principal component    
 barplot(100*p.variance.explained, las=2, xlab='', ylab='% Variance Explained')
- 
-
