b0e1e70f7ae80de5ff701cbfd89bc5c7a22e0c65
ManuelDelpero <manueldlpro94@gmail.com>
2020-01-23 11:45:40 +0100

    updates

--- Plots/ResultsPlots.R
+++ Plots/ResultsPlots.R
@@ -20,22 +20,27 @@ chromosomes <- c(1:19, "X", "Y")
 
 annotation <- c()
 for(chr in chromosomes){
-  annotation <- rbind(annotation, map[map[,"chr"] == chr,])
+  annotation <- rbind(annotation, markerannot[markerannot[,"Chromosome"] == chr,])
 }
 
 lodmatrixDOM <- lodmatrixDOM[rownames(annotation),]
 lodmatrixADD <- lodmatrixADD[rownames(annotation),]
-lodmatrixADDDOM <- lodmatrixADDDOM[rownames(annotation),]
+lodmatrixADDDOM <- mprofiles[rownames(annotation),]
 
 # Preliminary visual check bodyweight 
 bw <- mprofiles[,c(1:32)]
 rotate <- function(x) t(apply(x, 2, rev))
 image(rotate(bw))
 lodannotmatrix <- cbind(annotation[rownames(lodmatrixADDDOM), ], lodmatrixADDDOM)
-chr17 <- lodannotmatrix[which(lodannotmatrix[,"chr"] == 17),]
+chr17 <- lodannotmatrix[which(lodannotmatrix[,"Chromosome"] == 17),]
 chr17ord <- chr17[order(chr17[,2], decreasing = FALSE),]
-plot(main = "QTL Gon weight [Chr 17]", c(min(as.numeric(chr17ord[, "bp_mm10"])), max(as.numeric(chr17ord[, "bp_mm10"]))), c(0,9), ylab = "-log10 [pvalue]", xlab = "Position [mb]", las = 2, t = "n", xaxt = "n")
-points(x = as.numeric(chr17ord[,"bp_mm10"]), y = chr17ord[,"Gon"] , type = "l", col="dodgerblue", lwd = 1)
+# Gon chr 17
+plot(main = "QTL Gon weight [Chr 17]", c(min(as.numeric(chr17ord[, "Position"])), max(as.numeric(chr17ord[, "Position"]))), c(0,9), ylab = "-log10 [pvalue]", xlab = "Position [mb]", las = 2, t = "n", xaxt = "n")
+points(x = as.numeric(chr17ord[,"Position"]), y = chr17ord[,"Gon"] , type = "l", col="dodgerblue", lwd = 1)
+
+# Liver chr 17
+plot(main = "QTL liver weight [Chr 17]", c(min(as.numeric(chr17ord[, "Position"])), max(as.numeric(chr17ord[, "Position"]))), c(0,9), ylab = "-log10 [pvalue]", xlab = "Position [mb]", las = 2, t = "n", xaxt = "n")
+points(x = as.numeric(chr17ord[,"Position"]), y = chr17ord[,"Leber"] , type = "l", col="dodgerblue", lwd = 1)
 
 ## Manhattan plots (Plot the effect with the highest  lod score and use three different colors for each one) 
 chrs <- c(1:19,"X")
