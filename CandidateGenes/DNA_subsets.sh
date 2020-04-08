9bd895565e2038e8c54b7e2c7c3710b5fe510b5d
ManuelDelpero <manueldlpro94@gmail.com>
2020-04-08 16:10:31 +0200

    Combined the 3 subsets

--- CandidateGenes/DNA_subsets.sh
+++ CandidateGenes/DNA_subsets.sh
@@ -35,14 +35,23 @@ samtools index chr3_S2.bam
 samtools index chr17_S1
 
 # S12 subset and merge
-samtools view -h /home/danny/NAS/Mouse/DNA/Sequencing/MouseGenomeProject/BAM/ext_L7258-1_BFMI860-S12_S1_RP_trimmed.aligned.sorted.realigned.recalibrated.bam 17:7725897-26054796 > chr17.bam
-samtools view -h /home/danny/NAS/Mouse/DNA/Sequencing/MouseGenomeProject/BAM/ext_L7258-1_BFMI861-S12_S1_RP_trimmed.aligned.sorted.realigned.recalibrated.bam 15:66188210-78279750 > chr15.bam
-samtools view -h /home/danny/NAS/Mouse/DNA/Sequencing/MouseGenomeProject/BAM/ext_L7258-1_BFMI861-S12_S1_RP_trimmed.aligned.sorted.realigned.recalibrated.bam 3:93516258-101097858 > chr3.bam
+samtools view -h /home/danny/NAS/Mouse/DNA/Sequencing/MouseGenomeProject/BAM/ext_L7258-1_BFMI860-S12_S1_RP_trimmed.aligned.sorted.realigned.recalibrated.bam 17:7725897-26054796 > chr17S12_S1.bam
+samtools view -h /home/danny/NAS/Mouse/DNA/Sequencing/MouseGenomeProject/BAM/ext_L7258-1_BFMI860-S12_S1_RP_trimmed.aligned.sorted.realigned.recalibrated.bam 15:66188210-78279750 > chr15S12_S1.bam 
+samtools view -h /home/danny/NAS/Mouse/DNA/Sequencing/MouseGenomeProject/BAM/ext_L7258-1_BFMI860-S12_S1_RP_trimmed.aligned.sorted.realigned.recalibrated.bam 3:93516258-101097858 > chr3S12_S1.bam
 
-samtools view -h /home/danny/NAS/Mouse/DNA/Sequencing/MouseGenomeProject/BAM/ext_L7258-2_BFMI861-S12_S2_RP_trimmed.aligned.sorted.realigned.recalibrated.bam 17:7725897-26054796 > chr17S1_S4.bam
-samtools view -h /home/danny/NAS/Mouse/DNA/Sequencing/MouseGenomeProject/BAM/ext_L7258-2_BFMI861-S12_S2_RP_trimmed.aligned.sorted.realigned.recalibrated.bam 15:66188210-78279750 > chr15S1_S4.bam
-samtools view -h /home/danny/NAS/Mouse/DNA/Sequencing/MouseGenomeProject/BAM/ext_L7258-2_BFMI861-S12_S2_RP_trimmed.aligned.sorted.realigned.recalibrated.bam 3:93516258-101097858 > chr3S1_S4.bam
+samtools view -h /home/danny/NAS/Mouse/DNA/Sequencing/MouseGenomeProject/BAM/ext_L7258-2_BFMI860-S12_S2_RP_trimmed.aligned.sorted.realigned.recalibrated.bam 17:7725897-26054796 > chr17S12_S2.bam 
+samtools view -h /home/danny/NAS/Mouse/DNA/Sequencing/MouseGenomeProject/BAM/ext_L7258-2_BFMI860-S12_S2_RP_trimmed.aligned.sorted.realigned.recalibrated.bam 15:66188210-78279750 > chr15S12_S2.bam
+samtools view -h /home/danny/NAS/Mouse/DNA/Sequencing/MouseGenomeProject/BAM/ext_L7258-2_BFMI860-S12_S2_RP_trimmed.aligned.sorted.realigned.recalibrated.bam 3:93516258-101097858 > chr3S12_S2.bam
 
-samtools merge chr17_S1 chr17S1_S2.bam chr17S1_S4.bam 
-samtools merge chr15_S1 chr15S1_S2.bam chr15S1_S4.bam 
-samtools merge chr3_S1 chr3S1_S2.bam chr3S1_S4.bam 
+samtools merge chr17_S12.bam chr17S12_S1.bam chr17S12_S2.bam
+samtools merge chr15_S12.bam chr15S12_S1.bam chr15S12_S2.bam  
+samtools merge chr3_S12.bam chr3S12_S1.bam chr3S12_S2.bam
+
+samtools index chr17_S12.bam && samtools index chr15_S12.bam && samtools index chr3_S12.bam
+
+# merge the three bam files for each line subset and merge
+samtools merge chr17153_S12.bam chr17_S12.bam chr15_S12.bam chr3_S12.bam
+samtools merge chr17153_S1.bam chr17_S1.bam  chr15_S1.bam chr3_S1.bam 
+samtools merge chr17153_S2.bam chr17_S2.bam chr15_S2.bam chr3_S2.bam
+
+samtools index chr17153_S12.bam && samtools index chr17153_S1.bam && samtools index chr17153_S2.bam
