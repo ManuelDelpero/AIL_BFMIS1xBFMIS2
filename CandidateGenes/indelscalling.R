be7b7bd1ea5b7e1b06bc48e278ca8c4328900e41
ManuelDelpero <manueldlpro94@gmail.com>
2020-07-08 16:47:57 +0200

    function for INDELs calling

--- CandidateGenes/indelscalling.R
+++ CandidateGenes/indelscalling.R
@@ -12,12 +12,12 @@ execute <- function(x, intern = FALSE, execute = TRUE){
 }
 
 
-callINDELs <- function(bamfiles, chr = 1, startpos = 1, endpos = 2, outname = "myINDELs") {
+callINDELs <- function(bamfiles) {
   bamstr = paste0(bamfiles, collapse = " ") # Collapse all the bam files in a single string
   scalpel = "/home/florian/Downloads/scalpel-0.5.4/scalpel-discovery --single" # Location of scalpel executable
   reference = "/home/danny/References/Mouse/GRCm38_95/Mus_musculus.GRCm38.dna.toplevel.fa" #Reference genome
-  region = paste0(chr, ":", format(startpos, scientific = FALSE), "-", format(endpos, scientific = FALSE)) # Region requested in bed file
-  cmd <- paste0(scalpel, " --bam ", bamstr, " --ref ", reference, " --bed ", region, " --dir /home/manuel/AIL_S1xS2/RAWDATA/INDELsGenesGonLiver > ", outname, ".INDELs-filtered.vcf")
+  bedfile = "/home/manuel/AIL_S1xS2/RAWDATA/INDELsGenesGonLiverS1/bedfile.bed.txt"
+  cmd <- paste0(scalpel, " --bam ", bamstr, " --bed ", bedfile, " --ref ", reference)
   execute(cmd)
   invisible("")
 }
