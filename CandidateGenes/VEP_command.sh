ec66bf2291b4296230a6017a309ac750a09e81de
ManuelDelpero <manueldlpro94@gmail.com>
2020-08-26 15:00:22 +0200

    annotation also for domains

--- CandidateGenes/VEP_command.sh
+++ CandidateGenes/VEP_command.sh
@@ -1,1 +1,1 @@
-/home/arends/ensembleVEP/ensembl-tools-release-84/scripts/variant_effect_predictor/variant_effect_predictor.pl  --species mus musculus --cache  -i out_sorted.vcf -o outputVEP.vcf --vcf --check_existing
+/home/arends/ensembleVEP/ensembl-tools-release-84/scripts/variant_effect_predictor/variant_effect_predictor.pl  --species mus musculus --cache  -i all_combined_sorted.vcf -o outputVEP.vcf --vcf --check_existing
