13f4b06857cbffe93115e2198af534ad0d3ca4de
ManuelDelpero <manueldlpro94@gmail.com>
2020-09-13 18:48:49 +0200

    Decision tree algorithm updates

--- CandidateGenes/DecisionTreePrioritization.R
+++ CandidateGenes/DecisionTreePrioritization.R
@@ -3,11 +3,16 @@
 # copyright (c) 2018-2021 - Brockmann group - HU Berlin Manuel Delpero & Danny Arends
 # 
 
-
 setwd("C:/Users/Manuel/Desktop/AIL_S1xS2/RAWDATA/SNPsGenesGonLiver/SNPsGenesMetS")
 
 FullList <- read.table("ForPrioritization.txt", sep = "\t", check.names = FALSE, header=TRUE)
+setwd("C:/Users/Manuel/Desktop/AIL_S1xS2/RAWDATA")
+Diffexprliver <- read.csv("DiffExprLiver.txt", sep = "\t", header = TRUE, check.names = FALSE)
+DiffexprGonadalfat <- read.csv("DiffExprGon.txt", sep = "\t", header = TRUE, check.names = FALSE)
+#DiffexprSkeletalmuscle <- read.csv("DiffExprMuscle.txt", sep = "\t", header = TRUE, check.names = FALSE)
+#DiffexprPankreas <- read.csv("DiffExprPankreas.txt", sep = "\t", header = TRUE, check.names = FALSE)
 FullList[, "GENE"] <- as.character(FullList[, "GENE"])
+FullList[, "GENE"] <- gsub(" ", "", FullList[, "GENE"])
 Candidates <- as.character(unique(FullList[, "GENE"]))
 Candidates <- Candidates[-grep("ENSM", Candidates)]
 Candidates <- Candidates[-1]
@@ -17,26 +22,32 @@ FullList[which(FullList[, "DOMAIN"] == ""), "DOMAIN"] <- NA
 write.table(FullList, file="fulllist.txt", sep = "\t", quote = FALSE, row.names=FALSE, na = "")
 
 # Score genes based on mutations 
-RankCandidates <- matrix(NA, nrow = length(Candidates), ncol = 1, dimnames = list(Candidates, "SCORE"))
+RankCandidates <- matrix(NA, nrow = length(Candidates), ncol = 6, dimnames = list(Candidates,c("AAchange", "Regulatory", "DOMAIN", "Expression", "Annotation", "SCORE")))
 Score <- 0
 for (gene in Candidates) {
   geneVar <- FullList[which(FullList[,"GENE"] == gene),] # first branch, if the gene contains a mutation (regulatory regions (UTR) or AA change mutation)
-  if ((("5_prime" %in% geneVar[, "TYPE"]) || ("3_prime" %in% geneVar[, "TYPE"])) && (!("missense_variant" %in% geneVar[, "TYPE"]))){ # if the gene contain mutation in regulatory region
-  Score <- 3
+  if ((("5_prime" %in% geneVar[, "TYPE"]) || ("3_prime" %in% geneVar[, "TYPE"])) && (!("missense_variant" %in% geneVar[, "TYPE"]))){ # if the gene contain mutation only in a regulatory region
+  Score = 3
+  RankCandidates[gene, "Regulatory"] = "+"
   }
   if (("missense_variant" %in% geneVar[, "TYPE"]) || ("splice_donor_variant" %in% geneVar[, "TYPE"]) ||  ("stop_gained" %in% geneVar[, "TYPE"]) || ("stop_lost" %in% geneVar[, "TYPE"])) { # if the gene contains a mutation in the coding sequence
-    if (any(!(is.na(geneVar[, "DOMAIN"])))){ # if the gene contains a mutation in a coding sequence located in a domain
-      Score <- 5
+    Score <- 3
+	if (("5_prime" %in% geneVar[, "TYPE"]) || ("3_prime" %in% geneVar[, "TYPE"])){
+	  RankCandidates[gene, "Regulatory"] = "+"
+	  }
+	if (any(!(is.na(geneVar[, "DOMAIN"])))){ # if the gene contains a mutation in a coding sequence located in a domain
+      Score = Score + 2
+	  RankCandidates[gene, c("AAchange", "DOMAIN")] = "+"
 	}else{
-	  score = 3
+	  score = Score
+	  RankCandidates[gene, "AAchange"] = "+"
 	  }
   }
-  RankCandidates[gene,] <- Score  
+  if ((geneVar[, "GENE"] %in% DiffexprGonadalfat[, "mgi_symbol"]) || (geneVar[, "GENE"] %in% Diffexprliver[, "mgi_symbol"])){
+	RankCandidates[gene, "Expression"] = "+"
+	Score = Score +2
+  }
+  RankCandidates[gene,"SCORE"] <- Score  
 }
-
-# Score the genes based on expression data
-
-# Score the genes based on annotation
-
-  
-     
+RankCandidates <- data.frame(RankCandidates[order(as.numeric(RankCandidates[,"SCORE"], decreasing = TRUE)),])
+# Score the genes based on annotation  
