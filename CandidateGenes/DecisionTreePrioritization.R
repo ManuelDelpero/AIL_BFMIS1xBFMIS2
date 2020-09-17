2e96e5441cd337c07469f94995ac62d2d4680ca1
ManuelDelpero <manueldlpro94@gmail.com>
2020-09-17 19:05:50 +0200

    updates for SIFT score

--- CandidateGenes/DecisionTreePrioritization.R
+++ CandidateGenes/DecisionTreePrioritization.R
@@ -5,7 +5,7 @@
 
 setwd("C:/Users/Manuel/Desktop/AIL_S1xS2/RAWDATA/SNPsGenesGonLiver/SNPsGenesMetS")
 
-FullList <- read.table("ForPrioritization.txt", sep = "\t", check.names = FALSE, header=TRUE)
+FullList <- read.table("annotationSNPsCandidateGenes_all.txt", sep = "\t", check.names = FALSE, header=TRUE)
 setwd("C:/Users/Manuel/Desktop/AIL_S1xS2/RAWDATA")
 Diffexprliver <- read.csv("DiffExprLiver.txt", sep = "\t", header = TRUE, check.names = FALSE)
 DiffexprGonadalfat <- read.csv("DiffExprGon.txt", sep = "\t", header = TRUE, check.names = FALSE)
@@ -49,59 +49,50 @@ pathway_ALLGENE<-ConvertedIDgenes(pathways)
 pathway_ALLGENE <- unique(unlist(pathway_ALLGENE, recursive = TRUE, use.names = FALSE))
 
 # Score genes based on mutations (Decision tree)
-RankCandidates <- matrix(NA, nrow = length(Candidates), ncol = 9, dimnames = list(Candidates,c("AAchange", "UTRs", "Promoter", "CTCF B-site", "Enhancer", "DOMAIN", "Expression", "Annotation", "SCORE")))
-Score <- 0
+RankCandidates <- matrix(NA, nrow = length(Candidates), ncol = 10, dimnames = list(Candidates,c("SIFT_del", "SIFT_tol", "UTRs", "Promoter", "CTCF B-site", "Enhancer", "DOMAIN", "Expression", "Annotation", "SCORE")))
 for (gene in Candidates) {
+  Score <- 0
   GeneInfo <- GenesInfo[which(GenesInfo[, "mgi_symbol"] == gene),]
   geneVar <- FullList[which((FullList[,"POS"] > GeneInfo[, "start_position"]) & (FullList[,"POS"] < GeneInfo[, "end_position"])),] 
-  if ((("5_prime" %in% geneVar[, "TYPE"]) || ("3_prime" %in% geneVar[, "TYPE"])) && (!("missense_variant" %in% geneVar[, "TYPE"]))){ # if the gene contain mutation only in a regulatory region
-  Score = 3
-  RankCandidates[gene, "UTRs"] = "+"
+  if ((("5_prime" %in% geneVar[, "TYPE"]) || ("3_prime" %in% geneVar[, "TYPE"]))){ # if the gene contain mutation only in a regulatory region
+  Score = 1
+  RankCandidates[gene, "UTRs"] = 1
   }
   if ((("regulatory" %in% geneVar[, "TYPE"]) && (!("missense_variant" %in% geneVar[, "TYPE"])))){
-  Score = 3
     if (length(grep("promoter", geneVar[,"GENE"])) > 0){
-	  RankCandidates[gene, "Promoter"] = "+"
+	  RankCandidates[gene, "Promoter"] = 3
+	  Score = Score + 3
 	}
 	if (length(grep("enhancer", geneVar[,"GENE"])) > 0){
-	RankCandidates[gene, "Enhancer"] = "+"
+      RankCandidates[gene, "Enhancer"] = 1
+	  Score = Score + 1
 	}
 	if (length(grep("enhancer", geneVar[,"GENE"])) > 0){
-	RankCandidates[gene, "CTCF B-site"] = "+"
+	  RankCandidates[gene, "CTCF B-site"] = 1
+	  Score = Score + 1
 	}
   }
-  if (("missense_variant" %in% geneVar[, "TYPE"]) || ("splice_donor_variant" %in% geneVar[, "TYPE"]) ||  ("stop_gained" %in% geneVar[, "TYPE"]) || ("stop_lost" %in% geneVar[, "TYPE"])) { # if the gene contains a mutation in the coding sequence
-    Score <- 3
-	if ((("5_prime" %in% geneVar[, "TYPE"]) || ("3_prime" %in% geneVar[, "TYPE"]))){ 
-    RankCandidates[gene, "UTRs"] = "+"
-	}
-    if ((("regulatory" %in% geneVar[, "TYPE"]) && (!("missense_variant" %in% geneVar[, "TYPE"])))){
-      if (length(grep("promoter", geneVar[,"GENE"])) > 0){
-	    RankCandidates[gene, "Promoter"] = "+"
+  if ((length(grep("missense_variant", geneVar[, "TYPE"]) > 0)) || ("splice_donor_variant" %in% geneVar[, "TYPE"]) ||  ("stop_gained" %in% geneVar[, "TYPE"]) || ("stop_lost" %in% geneVar[, "TYPE"])) { # if the gene contains a mutation in the coding sequence
+    if (length(grep("deleterious", geneVar[, "TYPE"]) > 0)){
+	  Score <- Score + 3 
+	  RankCandidates[gene, "SIFT_del"] = 3
+	  }else{
+	  Score <- Score + 1
+	  RankCandidates[gene, "SIFT_tol"] = 1
 	  }
-	  if (length(grep("enhancer", geneVar[,"GENE"])) > 0){
-	    RankCandidates[gene, "Enhancer"] = "+"
-	  }
-      if (length(grep("enhancer", geneVar[,"GENE"])) > 0){
-	  RankCandidates[gene, "CTCF B-site"] = "+"
-	  }
-    } 
 	if (any(!(is.na(geneVar[, "DOMAIN"])))){ # if the gene contains a mutation in a coding sequence located in a domain
-      Score = Score + 2
-	  RankCandidates[gene, c("AAchange", "DOMAIN")] = "+"
-	}else{
-	  score = Score
-	  RankCandidates[gene, "AAchange"] = "+"
+      Score = Score + 3
+	  RankCandidates[gene, "DOMAIN"] = 3
 	  }
   }
   if (length(geneVar[, "GENE"]) > 0) {
     if ((gene %in% DiffexprGonadalfat[, "mgi_symbol"]) || (gene %in% Diffexprliver[, "mgi_symbol"])){ # Check the expressions
-	  RankCandidates[gene, "Expression"] = "+"
-	  Score = Score +2
+	  RankCandidates[gene, "Expression"] = 2
+	  Score = Score + 2
 	}
   }
   if (gene %in% pathway_ALLGENE){	# Check the annotation
-    RankCandidates[gene, "Annotation"] = "+"
+    RankCandidates[gene, "Annotation"] = 1
 	Score = Score + 1
   }
   RankCandidates[gene,"SCORE"] <- Score  
@@ -118,6 +109,13 @@ RankCandidates <- RankCandidates[which(rownames(RankCandidates) %in% rownames(bi
 biomart.RankCandidates <- biomart.RankCandidates[rownames(RankCandidates),]
 RankCandidates <- cbind(biomart.RankCandidates, RankCandidates)
 
-RankCandidates <- RankCandidates[order(RankCandidates[, "chromosome_name"]),]
+RankCandidate <- RankCandidates[order(-RankCandidates[, "SCORE"], decreasing = FALSE),]
 
-write.table(RankCandidates, file = "CandidatesScores.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+chromosomes <- c(3,12,15,16,17)
+RankCandidates <- c()
+for(chr in chromosomes){
+  RankCandidates <- rbind(RankCandidates, RankCandidate[RankCandidate[,"chromosome_name"] == chr,])
+}
+
+
+write.table(RankCandidates, file = "CandidatesScores2.txt", sep = "\t", quote = FALSE, row.names = FALSE)
