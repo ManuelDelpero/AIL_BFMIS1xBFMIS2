7958203ff1c7dd096076a978e47c0412a1ddb7e8
ManuelDelpero <manueldlpro94@gmail.com>
2021-05-12 14:53:52 +0200

    Get length of mouse Chrs and plot them

--- QualityControl/genotypes.QC_adj.R
+++ QualityControl/genotypes.QC_adj.R
@@ -28,7 +28,7 @@ gts[gts == "U"] <- NA
 
 # Write out the raw genotypes
 gts <- t(gts)
-write.table(gts, "genotypes.raw2.txt", sep="\t", quote=FALSE)
+#write.table(gts, "genotypes.raw2.txt", sep="\t", quote=FALSE)
 
 gts1 <- read.table("genotypes.raw.txt", sep="\t")
 gts2 <- read.table("genotypes.raw2.txt", sep="\t")
@@ -43,19 +43,23 @@ for(x in 1:length(tbls)){
     }
   }
 }
+print(nrow(gts))
 
 # All missing
 idx <- which(apply(gts,1, function(x){sum(is.na(x)) == length(x)}))
 gts <- gts[-idx,]
+print(nrow(gts))
 
 # Not segregating
 idx <- which(apply(gts,1,function(x){length(table(x)) == 1}))
 gts <- gts[-idx,]
+print(nrow(gts))
 
 # More than 10 % missing data
 ismissing <- apply(apply(gts, 1, is.na),2,sum)
 tooMuchMissing <- names(which((ismissing / ncol(gts)) > 0.1))
 gts <- gts[-which(rownames(gts) %in% tooMuchMissing),]
+print(nrow(gts))
 
 tbls <- apply(gts, 1, table)
 
@@ -65,10 +69,18 @@ map <- map[rownames(gts),]
 chrs <- 1:21 
 names(chrs) <- c(1:19, "X", "Y")
 
-plot(c(1,21), c(0,200000000), t = 'n', xaxt = "n", las= 2, ylab = "Position (mb)", xlab = "Chr", yaxt = 'n')
-aa <- apply(map, 1, function(r) { points(x = chrs[r[1]], y = r[2], pch = "-"); })
+
+library("BSgenome.Mmusculus.UCSC.mm10")
+lengths = seqlengths(Mmusculus)
+mylengths = lengths[1:21]
+
+plot(c(1,21), c(0,200000000), main = "S1 versus S2", t = 'n', xaxt = "n", las= 2, ylab = "Position (mb)", xlab = "Chr", yaxt = 'n')
+aa <- apply(map, 1, function(r) { points(x = chrs[r[1]], y = r[2], pch = "-", col = "red"); })
 axis(1, at = chrs, names(chrs))
 axis(2, at = seq(0,200000000, 25000000), seq(0,200000000, 25000000)/1000000)
+for (x in 1:length(chrs)){
+  segments(chrs[x], 0, chrs[x], mylengths[x], col = "black")
+}
 
-write.table(gts, "genotypes.cleaned.txt", sep="\t", quote=FALSE)
-write.table(map, "map.cleaned.txt", sep="\t", quote=FALSE)
+#write.table(gts, "genotypes.cleaned.txt", sep="\t", quote=FALSE)
+#write.table(map, "map.cleaned.txt", sep="\t", quote=FALSE)
